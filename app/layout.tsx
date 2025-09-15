7a82b5f076761ca042c8fb96997f597ecc0e46c3
Dimskey <dimasbudi328@gmail.com>
2025-09-15 21:49:08 +0700

    none

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "porto Beneran",
+  title: "D'Folio",
   description: "Super Porto",
 };
 
